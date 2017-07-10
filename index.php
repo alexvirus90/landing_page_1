23e1f0389068deae475d8222ce3dc7b7d27fd1d1
alex <a.kom90@mail.ru>
2017-07-10 17:32:56 +0300

    создание меню

--- index.php
+++ index.php
@@ -4,21 +4,28 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Создание лендингов на заказ</title>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="description" content="Заказ на создание лендинга." />
-    <meta name="keywords" content="заказ лендинга, создание лендинга" />
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-    <link type="text/css" rel="stylesheet" href="/styles/main.css" />
-		<script type="text/javascript" src="/lib/jquery_3.2.1/jquery-3.2.1.min.js"></script>
-		<script type="text/javascript" src="/js/functions.js"></script>
-
+	<title>Создание лендингов на заказ</title>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Заказ на создание лендинга." />
+	<meta name="keywords" content="заказ лендинга, создание лендинга" />
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<link type="text/css" rel="stylesheet" href="/styles/main.css" />
+	<script type="text/javascript" src="/lib/jquery_3.2.1/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="/js/functions.js"></script>
 </head>
 <body>
-
+	<div id="menu">
+		<a href="#advantages">Наши преимущества</a>
+		<a href="#steps">Порядок работы</a>
+		<a href="#why">Достижение эффективности</a>
+		<a href="#works">Наши работы и цены</a>
+		<a href="#guarantee">Гарантия</a>
+		<a class="order" href="#">Оформить заявку</a>
+	</div>
 </body>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </html>
