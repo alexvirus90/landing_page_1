43262b3f9da0e7bb0bf632a06417ca146c14bb51
alex <a.kom90@mail.ru>
2017-07-10 20:57:26 +0300

    внесены изменения. Добавлены необходимые файлы

--- index.php
+++ index.php
@@ -2,22 +2,35 @@
 
 ?>
 <!doctype html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
 <html lang="en">
+<!--<![endif]-->
 <head>
-	<title>Создание лендингов на заказ</title>
 	<meta charset="UTF-8">
+	<title>Создание лендингов на заказ</title>
 	<meta name="viewport"
 				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="description" content="Заказ на создание лендинга." />
 	<meta name="keywords" content="заказ лендинга, создание лендинга" />
-	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-	<link type="text/css" rel="stylesheet" href="/styles/main.css" />
-	<script type="text/javascript" src="/lib/jquery_3.2.1/jquery-3.2.1.min.js"></script>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="/css/main.css" />
+	<link rel="stylesheet" href="/css/fonts.css" />
+	<link rel="stylesheet" href="/css/media.css" />
+	<link rel="stylesheet" href="lib/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="lib/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="lib/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="lib/owl-carousel/owl.carousel.css" />
+	<link rel="stylesheet" href="lib/countdown/jquery.countdown.css" />
+
 	<script type="text/javascript" src="/js/functions.js"></script>
 </head>
 <body>
+
 	<div id="menu">
 		<a href="#advantages">Наши преимущества</a>
 		<a href="#steps">Порядок работы</a>
@@ -26,6 +39,34 @@
 		<a href="#guarantee">Гарантия</a>
 		<a class="order" href="#">Оформить заявку</a>
 	</div>
+
+	<header>
+		<h1 class="wow bounceInLeft">Landing Page под ключ</h1>
+		<h2 class="wow bounceInRight">без предоплаты и с гарантией результата</h2>
+	</header>
+
+	<!--[if lt IE 9]>
+	<script src="lib/html5shiv/es5-shim.min.js"></script>
+	<script src="lib/html5shiv/html5shiv.min.js"></script>
+	<script src="lib/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="lib/respond/respond.min.js"></script>
+	<![endif]-->
+	<script src="lib/jquery/jquery-3.2.1.min.js"></script>
+	<script src="lib/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script src="lib/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="lib/waypoints/waypoints-1.6.2.min.js"></script>
+	<script src="lib/scrollto/jquery.scrollTo.min.js"></script>
+	<script src="lib/owl-carousel/owl.carousel.min.js"></script>
+	<script src="lib/countdown/jquery.plugin.js"></script>
+	<script src="lib/countdown/jquery.countdown.min.js"></script>
+	<script src="lib/countdown/jquery.countdown-ru.js"></script>
+	<script src="lib/landing-nav/navigation.js"></script>
+	<script src="lib/inputmask/inputmask.js"></script>
+	<script src="lib/inputmask/jquery.inputmask.js"></script>
+	<script src="lib/wow/wow.min.js"></script>
+
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
 </body>
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
 </html>
