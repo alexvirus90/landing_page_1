9dad8fe085b226f6c48e75073c906bdb08cdb84a
alex <a.kom90@mail.ru>
2017-07-11 02:16:05 +0300

    Исправление бага со сдвигом фиксированного меню. Добавлено расширение "fancybox".

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,29 @@
 const DELAY = 500;
 
 $(document).ready(function() {
+
+	//Исправление бага со сдвигом фиксированного меню
+	$("#menu, body").css("width", $(window).width() + "px");
+
+	$(window).on("resize", function() {
+		$("#menu, body").css("width", $(window).width() + "px");
+	});
+
+	//Плавный переход через меню по сайту
+	$("#menu a:not(a.order)").on("click", function() {
+		$("html, body").stop().animate({
+			scrollTop: $($(this).attr("href")).offset().top - $("#menu").innerHeight()
+		}, DELAY);
+	});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".site a").fancybox({
+		'minWidth'  : 900
+	});
+
 	$("#popup_close img").on("click", function() {
 		$("#parent_popup").hide(DELAY);
 	});
