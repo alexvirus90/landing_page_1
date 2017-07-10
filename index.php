da37db3453b3f9726d1c5985840b349c2be540fa
alex <a.kom90@mail.ru>
2017-07-10 17:19:55 +0300

    Подготовка и предворительная настройка. Добавлены и созданы необходимые файлы.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Создание лендингов на заказ</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Заказ на создание лендинга." />
+    <meta name="keywords" content="заказ лендинга, создание лендинга" />
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    <link type="text/css" rel="stylesheet" href="/styles/main.css" />
+    <link type="text/css" rel="stylesheet" href="/styles/animate.css" />
+    <link type="text/css" rel="stylesheet" href="/fancybox/jquery.fancybox.css" />
+		<script type="text/javascript" src="/lib/jquery_3.2.1/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="/js/functions.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
